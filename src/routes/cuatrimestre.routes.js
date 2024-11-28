49e4d48ab81ac5f7b422267b49a5f01ea4980758
DearWawuis <jiajcobaq110@gmail.com>
2024-11-28 13:31:17 -0600

    Modificados endpoints de cuatrimestres

--- src/routes/cuatrimestre.routes.js
+++ src/routes/cuatrimestre.routes.js
@@ -2,14 +2,15 @@ import express from 'express';
 import { getCuatrimestres, createCuatrimestre, getCuatrimestre, deleteCuatrimestre, getMateriasByCuatrimestre, createCalificacion, getCalificacionesByAlumno, verificarCalificacionExistente, getTopUsers } from '../controllers/cuatrimestre.controller';
 const router = express.Router();
 
-router.get('/:id_usuario', getCuatrimestres);
-router.post('/:id_usuario', createCuatrimestre);
-router.get('/:id_usuario/:cuatrimestre_id', getCuatrimestre);
-router.delete('/:id_usuario/:cuatrimestre_id', deleteCuatrimestre);
+router.get('/users/top3', getTopUsers);
 router.get('/materias/cuatri/:cuatrimestreNumero', getMateriasByCuatrimestre);
 router.post('/materias/calific', createCalificacion);
-router.get('/calific/materias/:id_usuario/:numero_cuatrimestre', getCalificacionesByAlumno);
 router.post('/calificaciones/verificar', verificarCalificacionExistente);
-router.get('/users/top3', getTopUsers);
+router.get('/calific/materias/:id_usuario/:numero_cuatrimestre', getCalificacionesByAlumno);
+router.get('/:id_usuario/:cuatrimestre_id', getCuatrimestre);
+router.delete('/:id_usuario/:cuatrimestre_id', deleteCuatrimestre);
+router.get('/:id_usuario', getCuatrimestres);
+router.post('/:id_usuario', createCuatrimestre);
+
 
 export default router;
